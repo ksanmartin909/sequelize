9db8fb773601a1c7baca9e3b0e822020b3b8650e
ksanmartin@filament.com <ksanmartin@filamentgames.com>
2021-06-11 11:09:43 -0700

    modularized connection and models

---
+++ Sequelize_ORM/connection.js
@@ -0,0 +1,21 @@
+
+const Sequelize = require("sequelize");
+const mysql2 = require('mysql2');
+require('dotenv').config();
+
+
+module.exports = new Sequelize(
+    'sequelizeTestDB', 
+    'admin',
+    process.env.DB_PASSWORD,
+    {
+        host: 'sequelize-test2-instance-1.c1vi0b0qtajg.us-east-2.rds.amazonaws.com',
+        port:3306,
+        dialect:"mysql",  dialectModule: mysql2, 
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+})
--- Sequelize_ORM/index.js
+++ Sequelize_ORM/index.js
@@ -1,39 +1,7 @@
-//const Customers = require("./src/models/Customers");
+const Customers = require("./src/models/Customers");
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-2'});
-const Sequelize = require("sequelize");
-const mysql2 = require('mysql2')
-require('dotenv').config()
-
-
-const connection = new Sequelize(
-    'sequelizeTestDB', 
-    'admin',
-    process.env.DB_PASSWORD,
-    {
-        host: 'sequelize-test2-instance-1.c1vi0b0qtajg.us-east-2.rds.amazonaws.com',
-        port:3306,
-        dialect:"mysql",  dialectModule: mysql2, 
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-})
-
-const Customers = connection.define('customers', {
-    id: {
-        type: Sequelize.INTEGER(),
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey:true
-    },
-
-    Name: Sequelize.STRING(),
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE
-});
+const connection = require('./connection');
 
 
 
