836510809c1753dec8ee95ae795d8b197f7eed70
ksanmartin@filament.com <ksanmartin@filamentgames.com>
2021-06-16 12:11:13 -0700

    all three working perfectly

--- Test_Url_Params/index.js
+++ Test_Url_Params/index.js
@@ -1,14 +1,16 @@
-exports.handler = async (event, context, callback) => {
+exports.handler =  (event, context, callback) => {
     const results ={
         'query_string': event['queryStringParameters']['query_string'],
         'url_params': event['pathParameters']['number'],
-        'req_body': event['body'],
-    }
+        'req_body': JSON.parse(event['body'])['name'],
+    };
+    
+  
     const response = {
         statusCode: 200,
-        body: JSON.stringify(results),
+          body: JSON.stringify(results)
     };
 
     console.log(event);
-    callback(null, response)
+    callback(null, response);
 };
