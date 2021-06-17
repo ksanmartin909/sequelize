71d9ad8f6d10d3c71f57a9abfcfd3a866780533e
ksanmartin@filament.com <ksanmartin@filamentgames.com>
2021-06-17 11:19:15 -0700

    cleanup index file

--- auto_migrations/index.js
+++ auto_migrations/index.js
@@ -1,17 +1,17 @@
 
 const connection = require('./database/connection');
-const Fishes = require('./models/fish')
 const Sequelize = require('sequelize');
+const Model = require('./models/fish')
+const Fish = Model(connection,Sequelize)
 
 exports.handler = function(event, context, callback){
     
     connection.sync({alter:true})
     .then(() => {
-        Fishes(connection,Sequelize)
         
-        connection.models.Fish.findOne({
+        Fish.findOne({
             where: {
-                id: 1
+                id: event['id']
             }
         }).then(user => {
                 callback(null, user)
